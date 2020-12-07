c6b949a2d6760cbe1e340eaa6232ce6bff94c9f2
Oksana.Pastukh <oksana.pastukh.itis.2020@lpnu.ua>
2020-12-07 12:55:59 +0200

    first

---
+++ Project9.1.A/project9.1.a.cpp
@@ -0,0 +1,135 @@
+//Lab_9.1.A
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <Windows.h> // забезпечення відображення кирилиці
+using namespace std;
+enum Specialty { ComputerScience, Informatics, Mathematics, Physics, Work };
+string specialtyStr[] = { "Комп'ютерні науки", "Інформатика", "Матем.та економ.","Фізика та інформ.","Трудове навчання" };
+struct Student
+{
+	string prizv;
+	unsigned course;
+	Specialty specialty;
+	unsigned grades_in_physics;
+	unsigned grades_in_mathematics;
+	unsigned grades_in_informatic;
+};
+void Create(Student* p, const int N);
+void Print(Student* p, const int N);
+int LineSearch_count(Student* p, const int N);
+double LineSearch_proc(Student* p, const int N);
+int main()
+{
+	SetConsoleCP(1251); // встановлення сторінки win-cp1251 в потік вводу
+	SetConsoleOutputCP(1251); // встановлення сторінки win-cp1251 в потік виводу
+
+	int N;
+	cout << "Введіть кількість студентів N: "; cin >> N;
+
+	Student* p = new Student[N];
+	double proc;
+	int menuItem;
+	do {
+		cout << endl << endl << endl;
+		cout << "Виберіть дію:" << endl << endl;
+		cout << " [1] - введення даних з клавіатури" << endl;
+		cout << " [2] - вивід даних на екран" << endl;
+		cout << " [3] - Кількість оцінок з програмуваннячий «5», «4», «3»:" << endl;
+		cout << " [4] - процент студентів, які отримали і з фізики і з математики оцінки «4» або «5»:" << endl;
+		cout << " [0] - вихід та завершення роботи програми" << endl << endl;
+		cout << "Введіть значення: "; cin >> menuItem;
+		cout << endl << endl << endl;
+		switch (menuItem)
+		{
+		case 1:
+			Create(p, N);
+			break;
+		case 2:
+			Print(p, N);
+			break;
+		case 3:
+			cout << "Кількість оцінок з програмуваннячий «5», «4», «3»:" << LineSearch_count(p, N) << endl;
+			break;
+		case 4:
+			proc = LineSearch_proc(p, N);
+			cout << "процент студентів, які отримали і з фізики і з математики оцінки «4» або «5»:" << endl;
+			cout << proc << "%" << endl;
+			break;
+		case 0:
+			break;
+		default:
+			cout << "Ви ввели помилкове значення! "
+				"Слід ввести число - номер вибраного пункту меню" << endl;
+		}
+	} while (menuItem != 0);
+	return 0;
+}
+void Create(Student* p, const int N)
+{
+	int specialty;
+	for (int i = 0; i < N; i++)
+	{
+		cout << "Студент № " << i + 1 << ":" << endl;
+		cin.get(); // очищуємо буфер клавіатури – бо залишаються символи
+		cin.sync(); // "кінець рядка", які не дають ввести наступний літерний рядок
+
+		cout << " прізвище: "; getline(cin, p[i].prizv);
+		cout << " курс: "; cin >> p[i].course;
+		cout << "  спеціальність (0 -Комп'ютерні науки , 1 -Інформатика, 2 -Матем. та економ., 3 - Фізика та інформ., 4 - Трудове навчання): ";
+		cin >> specialty;
+		p[i].specialty = (Specialty)specialty;
+		cout << "  оцінки з фізики: "; cin >> p[i].grades_in_physics;
+		cout << "  оцінки з математики: "; cin >> p[i].grades_in_mathematics;
+		cout << "  оцінки з інформатики: "; cin >> p[i].grades_in_informatic;
+	}
+}
+void Print(Student* p, const int N)
+{
+	cout << "=================================================================================================="
+		<< endl;
+	cout << "|№  |Прізвище  |Курс|Cпеціальність    |Oцінки з фізики|Oцінки з математики|Oцінки з інформатики |"
+		<< endl;
+	cout << "--------------------------------------------------------------------------------------------------"
+		<< endl;
+	for (int i = 0; i < N; i++)
+	{
+		cout << "|" << setw(2) << right << i + 1 << " ";
+		cout << "|" << setw(10) << left << p[i].prizv;
+		cout << "|" << setw(3) << right << p[i].course << " ";
+		cout << "|" << setw(17) << left << specialtyStr[p[i].specialty];
+		cout << "|" << setw(14) << right << p[i].grades_in_physics << " ";
+		cout << "|" << setw(18) << right << p[i].grades_in_mathematics << " ";
+		cout << "|" << setw(20) << right << p[i].grades_in_informatic << " " << " |" << endl;
+	}
+	cout << "=================================================================================================="
+		<< endl;
+	cout << endl;
+}
+int LineSearch_count(Student* p, const int N)
+{
+	int count = 0;
+	for (int i = 0; i < N; i++)
+	{
+		{
+			if (p[i].grades_in_informatic >= 3 && p[i].grades_in_informatic <= 5)
+			{
+				count++;
+			}
+		}
+	}
+	return  count;
+}
+double LineSearch_proc(Student* p, const int N)
+{
+	int count = 0;
+	for (int i = 0; i < N; i++)
+	{
+		if ((p[i].grades_in_physics >= 4 && p[i].grades_in_physics <= 5) &&
+			(p[i].grades_in_mathematics >= 4 && p[i].grades_in_mathematics <= 5))
+		{
+			count++;
+		}
+	}
+	return 100.0 * count / N;
+}
---
+++ UnitTest9.1.a/UnitTest9.1.a.cpp
@@ -0,0 +1,37 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+#include "../Project9.1.A/project9.1.a.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest91a
+{
+	TEST_CLASS(UnitTest91a)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			int c;
+			int N = 5;
+			Student* p = new Student[N];
+			p[0].prizv = "Pastukh";
+			p[0].specialty = ComputerScience;
+			p[0].grades_in_informatic = 5;
+			p[1].prizv = "Petrov";
+			p[1].specialty = Informatics;
+			p[1].grades_in_informatic = 4;
+			p[2].prizv = "Ivanov";
+			p[2].specialty = Mathematics;
+			p[2].grades_in_informatic = 3;
+			p[3].prizv = "Abramov";
+			p[3].specialty = ComputerScience;
+			p[3].grades_in_informatic = 2;
+			p[4].prizv = "Karginov";
+			p[4].specialty = Physics;
+			p[4].grades_in_informatic = 2;
+			c = LineSearch_count(p, N);
+			Assert::AreEqual(c, 3);
+		}
+	};
+}
