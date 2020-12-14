8b7afbd92f5a8cfacbd14ab6c09a095e96ae48fa
Oksana.Pastukh <oksana.pastukh.itis.2020@lpnu.ua>
2020-12-14 11:12:57 +0200

    test2

--- UnitTest9.1.a/UnitTest9.1.a.cpp
+++ UnitTest9.1.a/UnitTest9.1.a.cpp
@@ -12,26 +12,31 @@ namespace UnitTest91a
 		
 		TEST_METHOD(TestMethod1)
 		{
-			/*int c;
+			int c;
 			int N = 5;
 			Student* p = new Student[N];
 			p[0].prizv = "Pastukh";
 			p[0].specialty = ComputerScience;
-			p[0].grades_in_informatic = 5;
+			p[0].grades_in_physics = 5;
+			p[0].grades_in_mathematics = 4;
 			p[1].prizv = "Petrov";
 			p[1].specialty = Informatics;
-			p[1].grades_in_informatic = 4;
-			p[2].prizv = "Ivanov";
-			p[2].specialty = Mathematics;
-			p[2].grades_in_informatic = 3;
-			p[3].prizv = "Abramov";
-			p[3].specialty = ComputerScience;
-			p[3].grades_in_informatic = 2;
-			p[4].prizv = "Karginov";
-			p[4].specialty = Physics;
-			p[4].grades_in_informatic = 2;
-			c = LineSearch_count(p, N);
-			Assert::AreEqual(c, 3);*/
+			p[1].grades_in_physics = 3;
+			p[1].grades_in_mathematics = 2;
+			p[2].prizv = "Karginov";
+			p[2].specialty = Informatics;
+			p[2].grades_in_physics = 4;
+			p[2].grades_in_mathematics = 5;
+			p[3].prizv = "Ivanov";
+			p[3].specialty = Mathematics;
+			p[3].grades_in_physics = 2;
+			p[3].grades_in_mathematics = 2;
+			p[4].prizv = "Abramov";
+			p[4].specialty = Mathematics;
+			p[4].grades_in_physics = 3;
+			p[4].grades_in_mathematics = 3;
+			c = LineSearch_proc(p, N);
+			Assert::AreEqual(c, 40);
 		}
 	};
 }
